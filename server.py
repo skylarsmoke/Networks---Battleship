5a13b1df74aba37f224a6eb7f83b03dd2603f5a3
skylarsmoke <42879587+skylarsmoke@users.noreply.github.com>
2019-09-23 00:33:29 -0600

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,9 @@ import socket
 import sys
 import re
 
+#Ships health and associated char
+shipHealth = ["D", 2, "S", 3, "R", 3, "B", 4, "C", 5]
+
 #Creates "boards" to be filled by text files
 own_board = [[0 for x in range(10)] for y in range(10)]
 opponent_board = [["_" for x in range(10)] for y in range(10)]
@@ -25,9 +28,9 @@ def startServer(port, file):
     
     #Prints a display for both Own and Opponents board
     print()
-    print("        Own Board")
+    print("          Own Board")
     displayBoard(own_board)
-    print("     Opponent Board")
+    print("       Opponent Board")
     displayBoard(opponent_board)
     
     print("Server Started, waiting for client...")
@@ -56,22 +59,41 @@ def transferBoard(board, file):
                 board[x][y] = file.read(1)
                 
 def displayBoard(board):
-    line = "  _____________________"
-    for x in range(10):
+    line = "    _____________________"
+    n = 0
+    for x in range(9):
+        n += 1
         if x > 0:
-            line += "|\n | "
+            line += "|\n %d | " % n
         else:
-            line += "\n | "
+            line += "\n 0 | "
         for y in range(10):
             line += board[x][y] + " "
     print(line + "|")
-    print("  _____________________")
+    print("    _____________________")
+    print("     0 1 2 3 4 5 6 7 8 9 ")
     print()
     
 def checkHit(x, y):
-    #Ships health and associated char
-    shipHealth = ["D", 2, "S", 3, "R", 3, "B", 4, "C", 5]
     servMsg = ""
+    
+    if own_board[y][x] == "_":
+        servMsg = "hit=0" #means miss
+        own_board[y][x] = "O"
+        opponent_board[y][x] = "O"
+    else:
+        for i in range(10):
+            if own_board[y][x] == shipHealth[i]:
+                shipHealth[i + 1] -= 1
+                if shipHealth[i + 1] == 0:
+                    servMsg = "hit=1&sink=%s" %shipHealth[i]
+                else:
+                    servMsg = "hit=1"
+        own_board[y][x] = "X"
+        opponent_board[y][x] = "X"
+    return servMsg
+                    
+        
     
 def checkMsg(msg):
     servMsg = ""
@@ -84,9 +106,11 @@ def checkMsg(msg):
         for i in splitMsg:
             if re.search("x=\d&y=\d", i):
                 coord = re.split("&", i)
+                n = 0
                 for j in coord:
-                    val = re.search("\d", j).group()
-                    coord[j] = int(val)
+                    z = re.search("\d", j).group()
+                    coord[n] = int(z)
+                    n += 1
                 y = coord[1]
                 x = coord[0]
                 servMsg = checkHit(x, y)
